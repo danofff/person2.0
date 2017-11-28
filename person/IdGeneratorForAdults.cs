de319cb66e3dd549e15b5e7dab01ef429bc4eec6
dan <danofff@mail.ru>
2017-11-28 10:52:18 +0600

    edit ID generator, correct some mistakes

--- person/IdGeneratorForAdults.cs
+++ person/IdGeneratorForAdults.cs
@@ -15,9 +15,16 @@ namespace person
             ID randomId = new ID();
             DateTime idStart = new DateTime();
             randomId.Number=((""+rnd.Next(1000,10000))+rnd.Next(1000, 10000))+rnd.Next(1000, 10000);
-            while(!DateTime.TryParse(rnd.Next(1,31)+"."+rnd.Next(1,12)+"."+rnd.Next(DateTime.Now.AddYears(-10).Year, DateTime.Now.Year),out idStart))
+            while (true)
             {
+                while (!DateTime.TryParse(rnd.Next(1, 31) + "." + rnd.Next(1, 13) + "." + rnd.Next(DateTime.Now.AddYears(-10).Year, DateTime.Now.Year+1), out idStart))
+                {
 
+                }
+                if (idStart<DateTime.Now)
+                {
+                    break;
+                }
             }
             randomId.Date = idStart;
             randomId.ExpireDate = idStart.AddYears(10);
